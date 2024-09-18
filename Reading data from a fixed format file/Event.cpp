b420bc239505f5ee8e0d07e528f8cc42c0540e66
pankemort <psangtani17@gmail.com>
2024-09-18 23:58:34 +0530

    le

--- Reading data from a fixed format file/Event.cpp
+++ Reading data from a fixed format file/Event.cpp
@@ -1,12 +1,49 @@
-#include<iostream>
-#include<string>
-#include<stdio.h>
-#include<fstream>
-#include<list>
-#include<iterator>
-#include<sstream>
+// #include<iostream>
+// #include<string>
+// #include<stdio.h>
+// #include<fstream>
+// #include<list>
+// #include<iterator>
+// #include<sstream>
+// using namespace std;
+// class Event
+// {
+//         //fill your code here
+// };
+#include <iostream>
+#include <string>
+#include <list>
+#include <fstream>
+#include <sstream>
+
 using namespace std;
-class Event
-{
-        //fill your code here
+
+class Event {
+private:
+    string name;
+    string detail;
+    string type;
+    string organiser;
+
+public:
+    Event() {}
+
+    Event(string name, string detail, string type, string organiser) {
+        this->name = name;
+        this->detail = detail;
+        this->type = type;
+        this->organiser = organiser;
+    }
+
+    string getName() { return name; }
+    void setName(string name) { this->name = name; }
+
+    string getDetail() { return detail; }
+    void setDetail(string detail) { this->detail = detail; }
+
+    string getType() { return type; }
+    void setType(string type) { this->type = type; }
+
+    string getOrganiser() { return organiser; }
+    void setOrganiser(string organiser) { this->organiser = organiser; }
 };
--- Reading data from a fixed format file/EventBO.cpp
+++ Reading data from a fixed format file/EventBO.cpp
@@ -1,30 +1,64 @@
-#include<iostream>
-#include<string>
-#include<stdio.h>
-#include<fstream>
-#include<list>
-#include<iterator>
-#include"Event.cpp"
-#include<sstream>
+// #include<iostream>
+// #include<string>
+// #include<stdio.h>
+// #include<fstream>
+// #include<list>
+// #include<iterator>
+// #include"Event.cpp"
+// #include<sstream>
+// using namespace std;
+// class EventBO
+// {
+//         //fill your code here
+//          string* splitString(string str)//to read comma separated values
+//      {
+//         string* arr = new string[25];
+//         int i=0;
+//         string delimiter = ",";
+//         size_t pos = 0;
+//         string token;
+//         str+=",";
+//         while ((pos = str.find(delimiter)) != string::npos) 
+//         {
+//                 token = str.substr(0, pos);
+//                 arr[i]=token;
+//                 str.erase(0, pos + delimiter.length());
+//                 i++;
+//         }
+//             return arr; 
+//      }
+// };
+#include <list>
+#include <fstream>
+#include <sstream>
+#include "Event.cpp"
+
 using namespace std;
-class EventBO
-{
-        //fill your code here
-         string* splitString(string str)//to read comma separated values
-     {
-        string* arr = new string[25];
-        int i=0;
-        string delimiter = ",";
-        size_t pos = 0;
-        string token;
-        str+=",";
-        while ((pos = str.find(delimiter)) != string::npos) 
-        {
-                token = str.substr(0, pos);
-                arr[i]=token;
-                str.erase(0, pos + delimiter.length());
-                i++;
+
+class EventBO {
+public:
+    list<Event> readEventlist(ifstream& file) {
+        list<Event> events;
+        string line;
+        while (getline(file, line)) {
+            stringstream ss(line);
+            string name, detail, type, organiser;
+            getline(ss, name, ',');
+            getline(ss, detail, ',');
+            getline(ss, type, ',');
+            getline(ss, organiser, ',');
+            events.push_back(Event(name, detail, type, organiser));
         }
-            return arr; 
-     }
+        return events;
+    }
+
+    list<Event> searchEventlist(list<Event> events, string organiser) {
+        list<Event> result;
+        for (Event event : events) {
+            if (event.getOrganiser() == organiser) {
+                result.push_back(event);
+            }
+        }
+        return result;
+    }
 };
