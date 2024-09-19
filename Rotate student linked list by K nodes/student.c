e5d7612c33c763ca10617840b9866a458f467140
pankemort <psangtani17@gmail.com>
2024-09-19 14:11:10 +0530

    added 2nd question

---
+++ Rotate student linked list by K nodes/student.c
@@ -0,0 +1,18 @@
+#include "student.h"
+#include<stdlib.h>
+#include<string.h>
+
+void append(struct student s)
+{
+    //Fill your code here
+}
+
+void rotate(int k)
+{
+	//Fill your code here
+}
+
+void display()
+{
+	//Fill your code here
+}
